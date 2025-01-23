f5acbf906e9b053b2fe327daffe1215b6d02e2ef
Peter Nylander <peter.nylander@gmail.com>
2025-01-23 22:35:50 +0100

    Testing multiple authschemes

---
+++ src/ASPNETPlayground/Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "WhichAuthDoWeUse";
+    options.DefaultAuthenticateScheme = "WhichAuthDoWeUse";
+})
+    .AddJwtBearer("Bearer1", "", options => { options.IncludeErrorDetails = true; })
+    .AddJwtBearer("Bearer2", "", options => { options.IncludeErrorDetails = true; })
+    .AddPolicyScheme("WhichAuthDoWeUse", "", options =>
+    {
+        options.ForwardDefaultSelector = context =>
+        {
+            var authorization = context.Request.Headers.Authorization.ToString();
+            if (!string.IsNullOrEmpty(authorization) && authorization.StartsWith("Bearer "))
+            {
+                var token = authorization["Bearer ".Length..].Trim();
+                var jwtHandler = new JsonWebTokenHandler();
+
+                if (jwtHandler.CanReadToken(token))
+                {
+                    var issuer = jwtHandler.ReadJsonWebToken(token).Issuer;
+                    if (issuer == "Test2")
+                    {
+                        return "Bearer2";
+                    }
+                }
+            }
+
+            return "Bearer1";
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", [Authorize(AuthenticationSchemes = "Bearer1, Bearer2")] () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.RequireAuthorization();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ src/JwtValidator/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+Console.WriteLine("Hello, World!");
+
+var jwt1 = "";
+var jwt2_1 = "";
+var jwt2_2 = "";
+
+var signingKeyBase64_1 = "";
+var signingKeyBase64_2 = "";
+
+await ValidateTokenAsync(jwt1, "Test1", signingKeyBase64_1);
+await ValidateTokenAsync(jwt2_1, "Test2", signingKeyBase64_2);
+await ValidateTokenAsync(jwt2_2, "Test2", signingKeyBase64_2);
+
+async Task<TokenValidationResult> ValidateTokenAsync(string token, string issuer, string signingKey)
+{
+    var jwtHandler = new JsonWebTokenHandler();
+    var jwtToken = jwtHandler.ReadJsonWebToken(token);
+    var validationResult = await jwtHandler.ValidateTokenAsync(token, new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(signingKey))
+    });
+
+    Console.WriteLine($"Token issuer: {jwtToken.Issuer}");
+    Console.WriteLine($"Token validated result: {validationResult.IsValid}");
+    return validationResult;
+}
